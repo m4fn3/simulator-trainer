36e86d1a71ceba067357f59de08991fb7557be7b
ethanarbuckle <ethan.a.arbuckle@gmail.com>
2025-05-23 20:35:43 -0700

    fix warning

--- simulator-trainer/Common/CommandRunner.h
+++ simulator-trainer/Common/CommandRunner.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CommandRunner : NSObject
 
-+ (BOOL)runCommand:(NSString *)command withArguments:(NSArray<NSString *> *)arguments cwd:(NSString * _Nullable)cwdPath stdoutString:(NSString * _Nullable *)stdoutString error:(NSError ** _Nullable)errorOut;
++ (BOOL)runCommand:(NSString *)command withArguments:(NSArray<NSString *> *)arguments cwd:(NSString * _Nullable)cwdPath stdoutString:(NSString * _Nullable * _Nullable)stdoutString error:(NSError ** _Nullable)errorOut;
 + (BOOL)runCommand:(NSString *)command withArguments:(NSArray<NSString *> *)arguments stdoutString:(NSString * _Nullable * _Nullable)stdoutString error:(NSError ** _Nullable)error;
 
 @end
