26d7ec161b59e7cb53d9d23de36fcdec80fa15b5
Ethan Arbuckle <ea@sourcetheorem.com>
2025-05-24 21:27:27 -0700

    simhacks

--- simulator-trainer/UI/AppDelegate.m
+++ simulator-trainer/UI/AppDelegate.m
@@ -5,10 +5,12 @@
 //  Created by Ethan Arbuckle on 4/28/25.
 //
 
+#import <Cocoa/Cocoa.h>
 #import "AppDelegate.h"
 #import <objc/runtime.h>
 #import <objc/message.h>
 #import <dlfcn.h>
+#import "CommandRunner.h"
 
 @interface AppDelegate ()
 @property (nonatomic, strong) id simulatorDelegate;
@@ -21,11 +23,21 @@
 }
 
 - (void)setupSimulator {
+    
+    NSString *xcodeDeveloperPath = nil;
+    [CommandRunner runCommand:@"/usr/bin/xcode-select" withArguments:@[@"--print-path"] stdoutString:&xcodeDeveloperPath error:nil];
+    if (!xcodeDeveloperPath || ![xcodeDeveloperPath hasSuffix:@"/Contents/Developer"]) {
+        NSLog(@"Xcode Developer path is not set correctly.");
+        return;
+    }
+    
+    NSString *simulatorPath = [xcodeDeveloperPath stringByAppendingPathComponent:@"Applications/Simulator.app"];
+    NSString *simulatorExecutablePath = [simulatorPath stringByAppendingPathComponent:@"Contents/MacOS/Simulator"];
+    
     Method mainBundleMethod = class_getClassMethod([NSBundle class], @selector(mainBundle));
     IMP originalMainBundleIMP = method_getImplementation(mainBundleMethod);
     IMP swizzledMainBundleIMP = imp_implementationWithBlock(^(id _self) {
-        
-        NSBundle *simulatorBundle = [NSBundle bundleWithPath:@"/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/"];
+        NSBundle *simulatorBundle = [NSBundle bundleWithPath:simulatorPath];
         if (simulatorBundle) {
             return simulatorBundle;
         }
@@ -38,28 +50,15 @@
     Method boolForKeyMethod = class_getInstanceMethod([NSUserDefaults class], @selector(boolForKey:));
     IMP originalBoolForKeyIMP = method_getImplementation(boolForKeyMethod);
     IMP swizzledBoolForKeyIMP = imp_implementationWithBlock(^(id _self, NSString *key) {
-        if ([key isEqualToString:@"UseMonobar"]) {
-            return NO;
-        }
-        
-        if ([@[@"CarPlayExtraOptions", @"ShowTestingDevices", @"DebugLogging", @"ShowUITestMenu", @"ShowFPS"] containsObject:key]) {
+    
+        if ([@[@"CarPlayExtraOptions", @"DebugLogging", @"ShowFPS"] containsObject:key]) {
             return YES;
         }
         
         BOOL result = ((BOOL (*)(id, SEL, NSString *))originalBoolForKeyIMP)(_self, @selector(boolForKey:), key);
-        NSLog(@"boolForKey: %@ -> %d", key, result);
         return result;
     });
     method_setImplementation(boolForKeyMethod, swizzledBoolForKeyIMP);
-    
-    Method stringForKeyMethod = class_getInstanceMethod([NSUserDefaults class], @selector(stringForKey:));
-    IMP originalStringForKeyIMP = method_getImplementation(stringForKeyMethod);
-    IMP swizzledStringForKeyIMP = imp_implementationWithBlock(^(id _self, NSString *key) {
-        NSString *result = ((NSString *(*)(id, SEL, NSString *))originalStringForKeyIMP)(_self, @selector(stringForKey:), key);
-        NSLog(@"stringForKey: %@ -> %@", key, result);
-        return result;
-    });
-    method_setImplementation(stringForKeyMethod, swizzledStringForKeyIMP);
     
     void *simhandle = dlopen("/Users/ethanarbuckle/Desktop/Simulator", 0);
     if (simhandle == NULL) {
@@ -67,10 +66,38 @@
         return;
     }
     
+    Class _SimulatorDeviceWindow = objc_getClass("_TtC9Simulator12DeviceWindow");
+    SEL _performDragOperationSelector = sel_registerName("performDragOperation:");
+    
+    Method performDragOperationMethod = class_getInstanceMethod(_SimulatorDeviceWindow, _performDragOperationSelector);
+    IMP originalPerformDragOperationIMP = method_getImplementation(performDragOperationMethod);
+    IMP newPerformDragOperationIMP = imp_implementationWithBlock(^(id _self, id <NSDraggingInfo> sender) {
+        
+        NSPasteboard *pasteboard = [sender draggingPasteboard];
+        NSString *draggedType = [[pasteboard types] firstObject];
+        if (!draggedType || ![draggedType isEqualToString:NSPasteboardTypeFileURL]) {
+            return NO;
+        }
+        
+        NSArray *files = [pasteboard readObjectsForClasses:@[[NSURL class]] options:nil];
+        if (files.count == 0 || ![files.firstObject isKindOfClass:[NSURL class]]) {
+            return NO;
+        }
+        
+        NSString *realPath = [[files firstObject] URLByResolvingSymlinksInPath].path;
+        if ([[realPath pathExtension] isEqualToString:@"deb"]) {
+            NSLog(@"Installing a tweak");
+            return NO;
+        }
+
+        return ((BOOL (*)(id, SEL, id))originalPerformDragOperationIMP)(_self, _performDragOperationSelector, sender);
+    });
+    method_setImplementation(performDragOperationMethod, newPerformDragOperationIMP);
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         Class _SimulatorAppDelegate = objc_getClass("SimulatorAppDelegate");
         if (!_SimulatorAppDelegate) {
-            NSLog(@"SimulatorAppDelegate class not found.");
+            NSLog(@"SimulatorAppDelegate class not found");
             return;
         }
         
